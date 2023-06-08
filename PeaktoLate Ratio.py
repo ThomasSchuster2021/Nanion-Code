68e0d32514ad52a931c318d8240534547ec41325
ThomasSchuster2021 <112416792+ThomasSchuster2021@users.noreply.github.com>
2023-06-08 15:00:04 -0500

    Update PeaktoLate Ratio.py

--- PeaktoLate Ratio.py
+++ PeaktoLate Ratio.py
@@ -28,9 +28,10 @@ while i < int(num_variants):
     while j < int(num_cells):
         file = input('Input MATLAB file name: ')
         if comp == 1:
-            file = 'Data/' + file
+            #file = "C:\\Users\\thoma\\OneDrive\\Documents\\Research Code\\Data\\" + file
+        file = "Data\\" + file
         else
-            file = 'Analysis Files/' + file
+            file = 'Analysis Files\\' + file
         cell = int(input('Which cell? '))
         mat_files.append(file)
         cell_nums.append(cell)
